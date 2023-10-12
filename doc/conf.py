934f205a73be93839dfd1897094ed1a0955cea26
JacksonCampolattaro <jackcamp@vt.edu>
2023-10-12 18:54:38 +0200

    Directly include all CSS files

--- doc/conf.py
+++ doc/conf.py
@@ -2,12 +2,11 @@ DOXYFILE = 'Doxyfile-mcss'
 
 STYLESHEETS = [
     'https://fonts.googleapis.com/css?family=Inter:400,400i,700,700i|JetBrains+Mono:400,400i,600',
-    # '../css/m-theme-light.css',
-    # '../css/m-layout.css',
-    # '../css/m-grid.css',
-    # '../css/m-components.css',
-    # '../css/pygments-console.css',
-    '../css/m-light.css',
+    '../css/m-theme-light.css',
+    '../css/m-layout.css',
+    '../css/m-grid.css',
+    '../css/m-components.css',
+    '../css/pygments-console.css',
     '../css/m-documentation.css',
     '@CMAKE_CURRENT_SOURCE_DIR@/../doc/style.css',
 ]
